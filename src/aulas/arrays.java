b63a0cb9c3b3ffdfeaa756cc028f8dee562d9ca2
larissaloureiro <80994671+larissaloureiro@users.noreply.github.com>
2022-03-18 22:41:21 -0300

    Add arrays

---
+++ src/aulas/arrays.java
@@ -0,0 +1,45 @@
+package aulas;
+
+public class arrays {
+
+	public static void main(String[] args) {
+		
+		// ARRAY UNIDIMENSIONAL (VETOR)
+		int[] idades = {1, 6, 7, 80, 45}; //já está inicializado, pq já tem valores
+		int[] idades2 = new int[5]; //não possui dados escritos pelo usuário
+
+		String[] nomes = {"Julia", "Marcia", "Ana"};
+		String[] nomes2 = new String[3];
+		
+		System.out.println("Idade: " + idades[2] + " - Nome: " + nomes[2]);
+		
+		for (int i = 0; i < idades.length; i++) {
+			System.out.println(idades[i]);
+		}
+		
+		for (int i = 0; i < nomes.length; i++) {
+			System.out.println(nomes[i]);
+		}
+		
+		
+		// ARRAY MULTIDIMENSIONAL (MATRIZ)
+		
+		int [][] numeros = new int [3][3];
+		int valor = 1;
+		
+		for (int linha = 0; linha < 3; linha++) {
+			for (int coluna = 0; coluna < 3; coluna++) {
+				numeros[linha][coluna] = valor++;
+			}
+		}
+		
+		for (int linha = 0; linha < 3; linha++) {
+			for (int coluna = 0; coluna < 3; coluna++) {
+				System.out.print(numeros[linha][coluna] + "\t");
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
