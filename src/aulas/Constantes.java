c1ffb19182c7024638b2c29abccd779e29d66920
Larissa Loureiro <larissaloureiro87@gmail.com>
2022-03-23 19:00:07 -0300

    Update Constantes

--- src/aulas/Constantes.java
+++ src/aulas/Constantes.java
@@ -3,7 +3,7 @@ package aulas;
 public class Constantes {
 
 	public static void main(String[] args) {
-		// Para declarar constantes utiliza-se o nome todo em leras maiúsculas
+		// Para declarar constantes utiliza-se o nome todo em letras maiúsculas
 		final double PI = 3.14159; //não pode ser alterado em tempo de execução
 		
 
